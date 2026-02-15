5709c0e912ef5eab070667cb4b71bf1839a9eda8
eniamza <65225917+Eniamza@users.noreply.github.com>
2026-02-15 22:04:35 +0600

    fix: temp disable navbar menus until pages are implemented

--- src/constants/navbarItems.js
+++ src/constants/navbarItems.js
@@ -4,31 +4,31 @@ const navbarItems = [
         href: '/',
         description: 'The PrePreReg we love is now in Oracle! Build your routine with ease and get the best possible schedule based on live data',
         enabled: true
-    },
-    {
-        title: 'Changelog',
-        href: '/changelog',
-        description: 'Updates and changes to the Oracle platform',
-        enabled: true
-    },
-    {
-        title: 'Swap',
-        href: '/swap',
-        description: 'A centralized platform to swap courses with other students. No more endless emails and waiting for replies!',
-        enabled: true
-    },
-    {
-        title: 'Review',
-        href: '/faculty-review',
-        description: 'Rate your faculty and provide feedback. Scrolling through useless reviews is a thing of the past!',
-        enabled: false
-    },
-    {
-        title: "Seat Status",
-        href: '/seat-status',
-        description: 'Get live updates on seat status for all courses. No more countless refreshes to know if you got in or not!',
-        enabled: true
     }
+    // {
+    //     title: 'Changelog',
+    //     href: '/changelog',
+    //     description: 'Updates and changes to the Oracle platform',
+    //     enabled: true
+    // },
+    // {
+    //     title: 'Swap',
+    //     href: '/swap',
+    //     description: 'A centralized platform to swap courses with other students. No more endless emails and waiting for replies!',
+    //     enabled: true
+    // },
+    // {
+    //     title: 'Review',
+    //     href: '/faculty-review',
+    //     description: 'Rate your faculty and provide feedback. Scrolling through useless reviews is a thing of the past!',
+    //     enabled: false
+    // },
+    // {
+    //     title: "Seat Status",
+    //     href: '/seat-status',
+    //     description: 'Get live updates on seat status for all courses. No more countless refreshes to know if you got in or not!',
+    //     enabled: true
+    // }
 ]
 
 export default navbarItems;
